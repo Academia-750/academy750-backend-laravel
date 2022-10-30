88bc9db78d63a57a8ffe90c45156013348e7e791
Raul Alberto Moheno Zavaleta <ramz.162025@gmail.com>
2022-10-30 15:55:34 -0500

    Refactorizar funcion para eliminar tokens expirados en el AuthProvider

--- app/Core/Services/AuthService.php
+++ app/Core/Services/AuthService.php
@@ -2,7 +2,33 @@
 
 namespace App\Core\Services;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 class AuthService
 {
+    public static string $table_name_manage_tokens = "personal_access_tokens";
 
+    public static function hasTableManageTokens (): bool
+    {
+        return Schema::hasTable(self::$table_name_manage_tokens);
+    }
+
+    public static function RemoveExpiredTokens (): void
+    {
+        $recordsTokenExpired = DB::table('personal_access_tokens')->where('expires_at', '<', Carbon::now())->pluck('id');
+
+        if ($recordsTokenExpired->count() > 0 ) {
+            foreach ($recordsTokenExpired as $token_id) {
+                DB::table('personal_access_tokens')->where('id', '=', $token_id)->delete();
+            }
+        }
+    }
+
+    public static function RemoveExpiredTokensAction (): void
+    {
+        if (self::hasTableManageTokens()) {
+            self::RemoveExpiredTokens();
+        }
+    }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Core\Services\AuthService;
 use Carbon\Carbon;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\DB;
@@ -30,16 +31,9 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Gate::guessPolicyNamesUsing(function ($model) {
-            return 'App\\Policies\\Api\\'. Pluralizer::plural(class_basename($model)).'Policy';
+            return 'App\\Policies\\Api\\v1\\'. Pluralizer::plural(class_basename($model)).'Policy';
         });
 
-        /* Por cada petición, antes de procesar la operación, eliminaremos los tokens que ya están expirados */
-        $recordsTokenExpired = DB::table('personal_access_tokens')->where('expires_at', '<', Carbon::now())->pluck('id');
-
-        if ($recordsTokenExpired->count() > 0 ) {
-            foreach ($recordsTokenExpired as $token_id) {
-                DB::table('personal_access_tokens')->where('id', '=', $token_id)->delete();
-            }
-        }
+        AuthService::RemoveExpiredTokensAction();
     }
 }
