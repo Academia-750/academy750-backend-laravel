724f7dae06415785f73b63acc15cc42d505df6ac
Raul Alberto Moheno Zavaleta <ramz.162025@gmail.com>
2022-11-06 23:40:59 -0500

    Remover rutas de exportar e importar del recurso Opposition

--- routes/api/v1/routes/oppositions.routes.php
+++ routes/api/v1/routes/oppositions.routes.php
@@ -11,8 +11,8 @@ Route::post('oppositions/create', [OppositionsController::class, 'create'])->nam
 Route::patch('oppositions/update/{opposition}', [OppositionsController::class, 'update'])->name('api.v1.oppositions.update');
 Route::delete('oppositions/delete/{opposition}', [OppositionsController::class, 'delete'])->name('api.v1.oppositions.soft-delete');
 Route::post('oppositions/mass-selection-action', [OppositionsController::class, 'mass_selection_for_action'])->name('api.v1.oppositions.massSelectionForAction');
-Route::post('oppositions/export', [OppositionsController::class, 'export_records'])->name('api.v1.oppositions.export');
-Route::post('oppositions/import', [OppositionsController::class, 'import_records'])->name('api.v1.oppositions.import');
+/*Route::post('oppositions/export', [OppositionsController::class, 'export_records'])->name('api.v1.oppositions.export');
+Route::post('oppositions/import', [OppositionsController::class, 'import_records'])->name('api.v1.oppositions.import');*/
 Route::get('oppositions/import/template', [OppositionsController::class, 'download_template_import_records'])->name('api.v1.oppositions.import.template');
 
 Route::get('/oppositions/records/archived', [OppositionsController::class, 'get_records_archived'])->name('api.v1.oppositions.archived.get');
