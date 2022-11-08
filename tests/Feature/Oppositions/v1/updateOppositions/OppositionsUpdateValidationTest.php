270ab4b7ae47d8788911c39344ae56b7217f3ae0
Raul Alberto Moheno Zavaleta <ramz.162025@gmail.com>
2022-11-07 21:33:32 -0500

    Implementar y Testear validacion para actualizar una Oposicion

--- tests/Feature/Oppositions/v1/updateOppositions/OppositionsUpdateValidationTest.php
+++ tests/Feature/Oppositions/v1/updateOppositions/OppositionsUpdateValidationTest.php
@@ -2,20 +2,85 @@
 
 namespace Tests\Feature\Oppositions\v1\updateOppositions;
 
+use App\Models\Opposition;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
 use Tests\TestCase;
+use Tests\traits\AuthServiceTraitTest;
 
 class OppositionsUpdateValidationTest extends TestCase
 {
     use RefreshDatabase;
-    /** @test */
-    public function it_can(): void
-    {
-        //$url = '';
+    use AuthServiceTraitTest;
 
-        // $response = $this->getJson($url);
+    /** @test */
+    public function name_must_not_be_a_longer_than_100_characters(): void
+    {
+        $opposition = Opposition::factory()->create();
+
+        $data = [
+            'name' => Str::random(101),
+            //'period' => '2022-exam-I'
+        ];
+
+        $url = route('api.v1.oppositions.update', [ 'opposition' => $opposition->getRouteKey() ]);
+
+        $response = $this->patchJson($url, $data);
 
         // asserts...
+
+        $response->assertUnprocessable()->assertJsonValidationErrorFor('name');
+
+        $this->assertDatabaseMissing('oppositions', $data);
+
+    }
+
+    /** @test */
+    public function name_must_be_unique(): void
+    {
+        $nameOpposition = 'Conocimientos tacticos';
+
+        Opposition::factory()->create([
+            'name' => $nameOpposition
+        ]);
+
+        $opposition = Opposition::factory()->create();
+
+        $data = [
+            'name' => $nameOpposition,
+            //'period' => '2022-exam-I'
+        ];
+
+        $url = route('api.v1.oppositions.update', [ 'opposition' => $opposition->getRouteKey() ]);
+
+        $response = $this->patchJson($url, $data);
+
+        // asserts...
+
+        $response->assertUnprocessable()->assertJsonValidationErrorFor('name');
+
+    }
+
+    /** @test */
+    public function period_must_not_be_a_longer_than_100_characters(): void
+    {
+        $opposition = Opposition::factory()->create();
+
+        $data = [
+            'period' => Str::random(101),
+            //'period' => '2022-exam-I'
+        ];
+
+        $url = route('api.v1.oppositions.update', [ 'opposition' => $opposition->getRouteKey() ]);
+
+        $response = $this->patchJson($url, $data);
+
+        // asserts...
+
+        $response->assertUnprocessable()->assertJsonValidationErrorFor('period');
+
+        $this->assertDatabaseMissing('oppositions', $data);
+
     }
 }
