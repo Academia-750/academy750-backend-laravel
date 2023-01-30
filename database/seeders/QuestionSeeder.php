12fc8e1cbdaa9d617cc6c2aed5ff8066a2326818
unknown <Raul Alberto Moheno Zavaleta>
2023-01-30 14:59:01 -0500

    Modificar numero de preguntas a generar seeder

--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -25,17 +25,17 @@ class QuestionSeeder extends Seeder
         $hidraulica = Topic::query()->firstWhere('id', '2368f180-4e30-4dc9-ac56-c0be2d43ee0a');
 
         if ($hidraulica) {
-            $this->registerQuestionsModel($hidraulica, " Explicacion..." , 'yes', 'no', 10, '(TIPO TEST)');
-            $this->registerQuestionsModel($hidraulica, " Explicacion..." , 'no', 'yes', 10, '(TIPO MEMORIA)');
-            $this->registerQuestionsModel($hidraulica, " Explicacion..." , 'yes', 'yes', 10, '(TIPO MEMORIA y TEST)');
+            $this->registerQuestionsModel($hidraulica, " Explicacion..." , 'yes', 'no', 3, '(TIPO TEST)');
+            $this->registerQuestionsModel($hidraulica, " Explicacion..." , 'no', 'yes', 3, '(TIPO MEMORIA)');
+            $this->registerQuestionsModel($hidraulica, " Explicacion..." , 'yes', 'yes', 3, '(TIPO MEMORIA y TEST)');
         }
 
         $incendiosForestales = Topic::query()->firstWhere('id', 'e5f6b510-f351-4a0e-9302-905f082c8d08');
 
         if ($incendiosForestales) {
-            $this->registerQuestionsModel($incendiosForestales, " Explicacion..." , 'yes', 'no', 10, '(TIPO TEST)');
-            $this->registerQuestionsModel($incendiosForestales, " Explicacion..." , 'no', 'yes', 10, '(TIPO MEMORIA)');
-            $this->registerQuestionsModel($incendiosForestales, " Explicacion..." , 'yes', 'yes', 10, '(TIPO MEMORIA y TEST)');
+            $this->registerQuestionsModel($incendiosForestales, " Explicacion..." , 'yes', 'no', 20, '(TIPO TEST)');
+            $this->registerQuestionsModel($incendiosForestales, " Explicacion..." , 'no', 'yes', 20, '(TIPO MEMORIA)');
+            $this->registerQuestionsModel($incendiosForestales, " Explicacion..." , 'yes', 'yes', 20, '(TIPO MEMORIA y TEST)');
         }
     }
 }
