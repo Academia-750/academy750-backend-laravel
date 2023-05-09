c93e78c2ca820ee465137a458d9cd38914153b19
Raul Alberto Moheno Zavaleta <ramz.162025@gmail.com>
2023-05-09 11:14:37 -0500

    .

--- database/migrations/stored_procedures/2022_12_21_200634_generate_questions_from_tests_procedure.php
+++ database/migrations/stored_procedures/2022_12_21_200634_generate_questions_from_tests_procedure.php
@@ -21,35 +21,97 @@ return new class extends Migration
             IN `tipo_cuestionario` VARCHAR(255),
             IN `numero_preguntas_solicitadas` INT
         )
-        BEGIN
-                DECLARE c INTEGER;
-
-                SET c := (SELECT COUNT(*) from questions p WHERE p.questionable_id IN
-                (SELECT testable_id FROM testables s, tests t WHERE t.id=s.test_id
-                AND t.id=id_test)
-                AND p.id NOT IN(SELECT m.question_id from question_test m, tests t
-                WHERE t.id=m.test_id AND t.user_id=id_usuario AND t.test_type=tipo_cuestionario
-                AND m.have_been_show_card_memory='yes')
-                AND p.its_for_card_memory='yes');
-
-                IF c < 1 THEN
-
-                update question_test m SET m.have_been_show_card_memory='no' WHERE m.question_id IN
-                (SELECT p.id from questions p WHERE p.questionable_id IN
-                (SELECT testable_id FROM testables s, tests t
-                WHERE t.id=s.test_id AND t.id=id_test AND t.user_id=id_usuario));
-
-                END IF;
-
-                SELECT p.id from questions p WHERE p.questionable_id IN
-                 (SELECT testable_id FROM testables s, tests t WHERE t.id=s.test_id
-                  AND t.user_id=id_usuario AND t.id=id_test) AND p.id NOT IN
-                  (SELECT m.question_id from question_test m, tests t
-                  WHERE t.id=m.test_id AND t.test_type=tipo_cuestionario AND
-                  t.user_id=id_usuario and m.have_been_show_card_memory='yes')
-                  ORDER BY RAND() LIMIT numero_preguntas_solicitadas;
-
-        END";
+        BEGIN DECLARE c INTEGER;
+SET
+  c := (
+    SELECT
+      COUNT(*)
+    from
+      questions p
+    WHERE
+      p.questionable_id IN (
+        SELECT
+          testable_id
+        FROM
+          testables s,
+          tests t
+        WHERE
+          t.id = s.test_id
+          AND t.id = id_test
+      )
+      AND p.id NOT IN(
+        SELECT
+          m.question_id
+        from
+          question_test m,
+          tests t
+        WHERE
+          t.id = m.test_id
+          AND t.user_id = id_usuario
+          AND t.test_type = tipo_cuestionario
+          AND m.have_been_show_card_memory = 'yes'
+      )
+      AND p.its_for_card_memory = 'yes'
+  );
+IF c < 1 THEN
+update
+  question_test m
+SET
+  m.have_been_show_card_memory = 'no'
+WHERE
+  m.question_id IN (
+    SELECT
+      p.id
+    from
+      questions p
+    WHERE
+      p.questionable_id IN (
+        SELECT
+          testable_id
+        FROM
+          testables s,
+          tests t
+        WHERE
+          t.id = s.test_id
+          AND t.id = id_test
+          AND t.user_id = id_usuario
+      )
+  );
+END IF;
+SELECT
+  p.id
+from
+  questions p
+WHERE
+  p.questionable_id IN (
+    SELECT
+      testable_id
+    FROM
+      testables s,
+      tests t
+    WHERE
+      t.id = s.test_id
+      AND t.user_id = id_usuario
+      AND t.id = id_test
+  )
+  AND p.id NOT IN (
+    SELECT
+      m.question_id
+    from
+      question_test m,
+      tests t
+    WHERE
+      t.id = m.test_id
+      AND t.test_type = tipo_cuestionario
+      AND t.user_id = id_usuario
+      and m.have_been_show_card_memory = 'yes'
+  )
+ORDER BY
+  RAND()
+LIMIT
+  numero_preguntas_solicitadas;
+END
+";
 
         $procedure2= "DROP PROCEDURE IF EXISTS `get_questions_by_test`;
         CREATE PROCEDURE `get_questions_by_test`(
@@ -58,37 +120,101 @@ return new class extends Migration
             IN `tipo_cuestionario` VARCHAR(255),
             IN `numero_preguntas_solicitadas` INT
         )
-        BEGIN
-                DECLARE c INTEGER;
-
-                SET c := (   SELECT Count(*) from questions p WHERE p.questionable_id IN
-                (SELECT testable_id FROM testables s, tests t WHERE t.id=s.test_id
-                 AND t.user_id=id_usuario AND t.id=id_test) AND p.id NOT IN
-                 (SELECT m.question_id from question_test m, tests t
-                 WHERE t.id=m.test_id AND t.test_type='test' AND
-                 t.user_id=id_usuario and m.have_been_show_test='yes')
-                 ORDER BY RAND() LIMIT numero_preguntas_solicitadas);
-
-                IF c < 1 THEN
-
-                update question_test m SET m.have_been_show_test='no' WHERE m.question_id IN
-                (SELECT p.id from questions p WHERE p.questionable_id IN
-                (SELECT testable_id FROM testables s, tests t
-                WHERE t.id=s.test_id AND t.id=id_test AND t.user_id=id_usuario));
-
-                END IF;
-
-
-
-                SELECT p.id from questions p WHERE p.questionable_id IN
-                 (SELECT testable_id FROM testables s, tests t WHERE t.id=s.test_id
-                  AND t.user_id=id_usuario AND t.id=id_test) AND p.id NOT IN
-                  (SELECT m.question_id from question_test m, tests t
-                  WHERE t.id=m.test_id AND t.test_type=tipo_cuestionario AND
-                  t.user_id=id_usuario and m.have_been_show_test='yes')
-                  ORDER BY RAND() LIMIT numero_preguntas_solicitadas;
-
-        END";
+        BEGIN DECLARE c INTEGER;
+SET
+  c := (
+    SELECT
+      Count(*)
+    from
+      questions p
+    WHERE
+      p.questionable_id IN (
+        SELECT
+          testable_id
+        FROM
+          testables s,
+          tests t
+        WHERE
+          t.id = s.test_id
+          AND t.user_id = id_usuario
+          AND t.id = id_test
+      )
+      AND p.id NOT IN (
+        SELECT
+          m.question_id
+        from
+          question_test m,
+          tests t
+        WHERE
+          t.id = m.test_id
+          AND t.test_type = 'test'
+          AND t.user_id = id_usuario
+          and m.have_been_show_test = 'yes'
+      )
+    ORDER BY
+      RAND()
+    LIMIT
+      numero_preguntas_solicitadas
+  );
+IF c < 1 THEN
+update
+  question_test m
+SET
+  m.have_been_show_test = 'no'
+WHERE
+  m.question_id IN (
+    SELECT
+      p.id
+    from
+      questions p
+    WHERE
+      p.questionable_id IN (
+        SELECT
+          testable_id
+        FROM
+          testables s,
+          tests t
+        WHERE
+          t.id = s.test_id
+          AND t.id = id_test
+          AND t.user_id = id_usuario
+      )
+  );
+END IF;
+SELECT
+  p.id
+from
+  questions p
+WHERE
+  p.questionable_id IN (
+    SELECT
+      testable_id
+    FROM
+      testables s,
+      tests t
+    WHERE
+      t.id = s.test_id
+      AND t.user_id = id_usuario
+      AND t.id = id_test
+  )
+  AND p.id NOT IN (
+    SELECT
+      m.question_id
+    from
+      question_test m,
+      tests t
+    WHERE
+      t.id = m.test_id
+      AND t.test_type = tipo_cuestionario
+      AND t.user_id = id_usuario
+      and m.have_been_show_test = 'yes'
+  )
+ORDER BY
+  RAND()
+LIMIT
+  numero_preguntas_solicitadas;
+END
+";
 
         DB::unprepared($procedure1);
         DB::unprepared($procedure2);
