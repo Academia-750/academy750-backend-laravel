eacc09b0a47b6db073101181b206eaa77b05fee4
Abel Bordonado <abelbordonado@gmail.com>
2023-08-24 05:42:00 +0200

    Acept Cookies missing API

--- routes/api/v1/index.php
+++ routes/api/v1/index.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\v1\UsersController;
+use App\Models\ManageUsersInformation;
 
-Route::prefix('v1')->group(static function(){
+Route::prefix('v1')->group(static function () {
     require __DIR__ . '/routes/json-api-auth.php';
     Route::post('/test/errors-validation/manually', static function (\Illuminate\Http\Request $request) {
         $validator = Validator::make($request->all(), [
@@ -29,6 +30,77 @@ Route::prefix('v1')->group(static function(){
             'message' => 'Welcome'
         ]);
     });
+    /**
+     * @group Guest
+     * Contact Us
+     */
+    Route::post('guest/user/contact-us', [UsersController::class, 'contactsUS'])->name('api.v1.users.home-page.form.contact-us');
+    /**
+     * @group Guest
+     * Hello World
+     */
+    Route::get('guest/user/hello', function () {
+        return response()->json([
+            'message' => 'Welcome'
+        ]);
+    });
+
+    /**
+     * @group Guest
+     * Accept Cookies
+     */
+    Route::post('guest/user/accept-cookies', function (\Illuminate\Http\Request $request) {
+        $userIp = ManageUsersInformation::query()
+            ->where('ip', $request->ip() ?? $request->getClientIp())
+            ->where('user_agent', $request->header('User-Agent'))
+            ->first();
+
+        if (!$userIp) {
+            ManageUsersInformation::query()->create([
+                'has_accept_cookies' => true,
+                'user_agent' => $request->header('User-Agent'),
+                'ip' => $request->ip() ?? $request->getClientIp()
+            ]);
+        } else {
+            $userIp->update([
+                'has_accept_cookies' => true,
+                'user_agent' => $request->header('User-Agent'),
+                'ip' => $request->ip() ?? $request->getClientIp()
+            ]);
+        }
+
+        return response()->json([
+            'has_accept_cookies' => true,
+            'user_agent' => $request->header('User-Agent'),
+            'ip' => $request->ip() ?? $request->getClientIp()
+        ]);
+    })->name('api.v1.users.home-page.form.accept-cookies');
+
+    /**
+     * @group Guest
+     * Has Accept Cookies
+     */
+    Route::post('guest/user/has-accept-cookies', function (\Illuminate\Http\Request $request) {
+        $userIp = ManageUsersInformation::query()
+            ->where('ip', $request->ip() ?? $request->getClientIp())
+            ->where('user_agent', $request->header('User-Agent'))
+            ->where('has_accept_cookies', true)
+            ->first();
+
+        if (!$userIp) {
+            return response()->json([
+                'has_accept_cookies' => false,
+                'user_agent' => $request->header('User-Agent'),
+                'ip' => $request->ip() ?? $request->getClientIp()
+            ]);
+        } else {
+            return response()->json([
+                'has_accept_cookies' => $userIp->has_accept_cookies,
+                'user_agent' => $request->header('User-Agent'),
+                'ip' => $request->ip() ?? $request->getClientIp()
+            ]);
+        }
+    })->name('api.v1.users.home-page.form.has-accept-cookies');
 
     Route::middleware(['auth:sanctum', 'only_users_with_account_enable'])->group(static function () {
         require __DIR__ . '/routes/profile.php';
@@ -46,6 +118,6 @@ Route::prefix('v1')->group(static function(){
         //require __DIR__ . '/routes/answers.routes.php';
         //require __DIR__ . '/routes/images.routes.php';
         require __DIR__ . '/routes/import-processes.routes.php';
-    // [EndOfLineMethodRegister]
+        // [EndOfLineMethodRegister]
     });
 });
